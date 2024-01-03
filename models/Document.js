4dd0fc54e3ee68c5dd9f8db4310ae8f6bda505bd
smanasek <66624699+Valentical@users.noreply.github.com>
2024-01-03 20:57:21 +0100

    commit #2

--- models/Document.js
+++ models/Document.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose')
+const shortid = require('shortid')
 
 const documentScheme = new mongoose.Schema({
     value: {
         type: String,
         required: true
-    }
+    },
+    _id: {
+        type: String,
+        default: shortid.generate
+    },
 })
 
 module.exports = mongoose.model("Document", documentScheme)
